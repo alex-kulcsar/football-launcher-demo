e9f222bda53df700e7548d68114001e422e2afda
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2024-09-17 12:19:44 -0400

    Finish extension prep.

--- launcher.ts
+++ launcher.ts
@@ -1,12 +1,14 @@
 namespace SpriteKind {
-    //% iskind
+    //% isKind
     export const P1Football = SpriteKind.create()
-    //% iskind
+    //% isKind
     export const P2Football = SpriteKind.create()
-    //% iskind
+    //% isKind
     export const P3Football = SpriteKind.create()
-    //% iskind
+    //% isKind
     export const P4Football = SpriteKind.create()
+    //% isKind
+    export const Target = SpriteKind.create()
 }
 
 interface FbLauncher {
@@ -23,11 +25,38 @@ namespace launcher {
     const DIRECTION_DIVISIONS: number = 8
     const MARKER_FILL_COLOR: number = 1
     const MARKER_BORDER_COLOR: number = 10
+    const FOOTBALL_KINDS: number[] = [
+        SpriteKind.P1Football,
+        SpriteKind.P2Football,
+        SpriteKind.P3Football,
+        SpriteKind.P4Football,
+    ]
 
     let launchers: FbLauncher[] = []
     let fbImage: Image = null
     let fbSpeed: number = 50
     let needsInit: boolean = true
+    let targetVelocities: number[][] = []
+    let currRound: number = -1
+    let targets: Sprite[] = []
+    let roundLength: number = 30
+    let targetSpeedup: number = 10
+
+    //% block
+    export function addRound(vx: number, vy: number): void {
+        let round: number[] = [vx, vy,]
+        targetVelocities.push(round)
+    }
+
+    //% block
+    export function addTarget(sprite: Sprite): void {
+        targets.push(sprite)
+    }
+
+    //% block
+    export function currentRound(): number {
+        return currRound + 1
+    }
 
     //% block
     //% player.defl=1
@@ -107,6 +136,27 @@ namespace launcher {
     }
 
     //% block
+    export function increaseTargetSpeed(target: Sprite): void {
+        if (target.vx < 0) {
+            target.vx -= targetSpeedup
+        }
+        if (target.vx > 0) {
+            target.vx += targetSpeedup
+        }
+        if (target.vy < 0) {
+            target.vy -= targetSpeedup
+        }
+        if (target.vy > 0) {
+            target.vy += targetSpeedup
+        }
+    }
+
+    //% block="number of rounds"
+    export function numRounds(): number {
+        return targetVelocities.length
+    }
+
+    //% block
     //% speed.defl = 25
     export function setFootballSpeed(speed: number): void {
         if (needsInit) {
@@ -129,14 +179,52 @@ namespace launcher {
         updateShadow(player)
     }
 
+    //% block
+    export function setRoundLength(seconds: number): void {
+        // Zero disables timer.
+        if (seconds >= 0) {
+            roundLength = seconds
+        }
+    }
+
+    //% block
+    export function startNewRound(): void {
+        if (currRound >= targetVelocities.length) {
+            return
+        }
+        currRound++
+        for (let k of FOOTBALL_KINDS) {
+            sprites.destroyAllSpritesOfKind(k)
+        }
+
+        let currVelocities: number[] = targetVelocities[currRound]
+        let vx: number = currVelocities[0]
+        let vy: number = currVelocities[1]
+        if (vx != 0 && Math.percentChance(50)) {
+            vx = 0 - vx
+        }
+        if (vy != 0 && Math.percentChance(50)) {
+            vy = 0 - vy
+        }
+        for(let s of targets) {
+            s.setPosition(80, 60)
+            s.setVelocity(vx, vy)
+            s.setFlag(SpriteFlag.BounceOnWall, true)
+        }
+
+        if (roundLength > 0) {
+            info.startCountdown(roundLength)
+        }
+    }
+
     function getAngle(player: number): number {
         /**
          *  PI        dir
          * --- * -------------
          *  2    NUM DIVISIONS
          */
-        // return Math.PI * launchers[player].direction / (DIRECTION_DIVISIONS << 1)
-        return Math.PI * launchers[player].direction / (DIRECTION_DIVISIONS * 2)
+        return Math.PI * launchers[player].direction / (DIRECTION_DIVISIONS << 1)
+        // return Math.PI * launchers[player].direction / (DIRECTION_DIVISIONS * 2)
     }
 
     function init(): void {
--- main.ts
+++ main.ts
@@ -4,56 +4,31 @@ namespace SpriteKind {
     export const FB3 = SpriteKind.create()
     export const FB4 = SpriteKind.create()
 }
-sprites.onOverlap(SpriteKind.FB4, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
-    info.player4.changeScoreBy(3)
-    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
-    if (targetSprite.vx < 0) {
-        targetSprite.vx += -10
-    } else if (targetSprite.vx > 0) {
-        targetSprite.vx += 10
-    }
-    if (targetSprite.vy < 0) {
-        targetSprite.vy += -10
-    } else if (targetSprite.vy > 0) {
-        targetSprite.vy += 10
-    }
-})
-sprites.onOverlap(SpriteKind.FB1, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
-    info.changeScoreBy(3)
-    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
-    if (targetSprite.vx < 0) {
-        targetSprite.vx += -10
-    } else if (targetSprite.vx > 0) {
-        targetSprite.vx += 10
-    }
-    if (targetSprite.vy < 0) {
-        targetSprite.vy += -10
-    } else if (targetSprite.vy > 0) {
-        targetSprite.vy += 10
-    }
-})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     launcher.launchFootball(1)
 })
 controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     launcher.launchFootball(2)
 })
+sprites.onOverlap(SpriteKind.P2Football, SpriteKind.Target, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    info.player2.changeScoreBy(3)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    launcher.increaseTargetSpeed(otherSprite)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     launcher.increaseAngle(1)
 })
+sprites.onOverlap(SpriteKind.P1Football, SpriteKind.Target, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    info.changeScoreBy(3)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    launcher.increaseTargetSpeed(otherSprite)
+})
 info.onCountdownEnd(function () {
-    if (round < fbVxValues.length - 1) {
-        round += 1
-        game.splash("Round " + (round + 1) + "!")
-        sprites.destroyAllSpritesOfKind(SpriteKind.FB1)
-        sprites.destroyAllSpritesOfKind(SpriteKind.FB2)
-        sprites.destroyAllSpritesOfKind(SpriteKind.FB3)
-        sprites.destroyAllSpritesOfKind(SpriteKind.FB4)
-        targetSprite.setPosition(80, 60)
-        targetSprite.setVelocity(fbVxValues[round], fbVyValues[round])
-        info.startCountdown(30)
+    if (launcher.currentRound() < launcher.numRounds()) {
+        launcher.startNewRound()
+        game.splash("Round " + launcher.currentRound() + "!")
     } else {
         game.gameOver(true)
     }
@@ -67,26 +42,23 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
     launcher.increaseAngle(2)
 })
-sprites.onOverlap(SpriteKind.FB2, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
-    info.player2.changeScoreBy(3)
-    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
-    if (targetSprite.vx < 0) {
-        targetSprite.vx += -10
-    } else if (targetSprite.vx > 0) {
-        targetSprite.vx += 10
-    }
-    if (targetSprite.vy < 0) {
-        targetSprite.vy += -10
-    } else if (targetSprite.vy > 0) {
-        targetSprite.vy += 10
-    }
-})
 controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     launcher.launchFootball(4)
 })
 controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
     launcher.decreaseAngle(2)
+})
+sprites.onOverlap(SpriteKind.P3Football, SpriteKind.Target, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    info.player3.changeScoreBy(3)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    launcher.increaseTargetSpeed(otherSprite)
+})
+sprites.onOverlap(SpriteKind.P4Football, SpriteKind.Target, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    info.player4.changeScoreBy(3)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    launcher.increaseTargetSpeed(otherSprite)
 })
 controller.player4.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
     launcher.decreaseAngle(4)
@@ -94,31 +66,12 @@ controller.player4.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pr
 controller.player3.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     launcher.launchFootball(3)
 })
-sprites.onOverlap(SpriteKind.FB3, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
-    info.player3.changeScoreBy(3)
-    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
-    if (targetSprite.vx < 0) {
-        targetSprite.vx += -10
-    } else if (targetSprite.vx > 0) {
-        targetSprite.vx += 10
-    }
-    if (targetSprite.vy < 0) {
-        targetSprite.vy += -10
-    } else if (targetSprite.vy > 0) {
-        targetSprite.vy += 10
-    }
-})
 controller.player4.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
     launcher.increaseAngle(4)
 })
 controller.player3.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
     launcher.increaseAngle(3)
 })
-let fbVyValues: number[] = []
-let fbVxValues: number[] = []
-let round = 0
-let targetSprite: Sprite = null
 game.showLongText("Use left and right to change your angle.\\n \\nPress A to launch a football.\\n \\nHow many times can you hit the target?", DialogLayout.Full)
 scene.setBackgroundColor(7)
 let p1Launcher = sprites.create(assets.image`p1_7`, SpriteKind.Player)
@@ -133,16 +86,14 @@ launcher.setPlayerSprite(p3Launcher, 3)
 let p4Launcher = sprites.create(assets.image`p4_0`, SpriteKind.Player)
 p4Launcher.setPosition(150, 100)
 launcher.setPlayerSprite(p4Launcher, 4)
-targetSprite = sprites.create(assets.image`target`, SpriteKind.Enemy)
-targetSprite.setBounceOnWall(true)
-let PI = Math.atan2(0, -1)
+let targetSprite = sprites.create(assets.image`target`, SpriteKind.Target)
+launcher.addTarget(targetSprite)
+launcher.addRound(50, 0)
+launcher.addRound(0, 50)
+launcher.addRound(50, 50)
 info.setScore(0)
 info.player2.setScore(0)
 info.player3.setScore(0)
 info.player4.setScore(0)
-round = 0
-fbVxValues = [50, 0, 50]
-fbVyValues = [0, 50, 50]
-targetSprite.setVelocity(fbVxValues[round], fbVyValues[round])
-game.splash("Round " + (round + 1) + "!")
-info.startCountdown(30)
+launcher.startNewRound()
+game.splash("Round " + launcher.currentRound() + "!")
